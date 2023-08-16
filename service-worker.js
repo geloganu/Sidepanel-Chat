562c1c919b43af9f913e19093f31682e3a0ed1ae
Logan <gelogan@uchicago.edu>
2023-08-16 15:00:27 -0400

    send to backend

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,54 @@
 // Allows users to open the side panel by clicking on the action toolbar icon
+
+function sendDataToBackend(tabId, content) {
+  const url = 'https://chrome-panel-backend-ff37a3d3fc79.herokuapp.com/send/text';
+  content = JSON.stringify(content); 
+  fetch(url, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: { tableId: tabId, content: content }
+  })
+  .then(response => response.json())
+  .then(responseData => {
+      console.log('Response from backend:', responseData);
+  })
+  .catch(error => {
+      console.error('Error sending data:', error);
+  });
+}
+
+
+function fetchUrl(tabId,url) {
+  // Function logic goes here
+  fetch(url,{method: 'GET'})
+      .then(response => response.text())
+      .then(html => {
+        // Parse the HTML content using DOMParser
+        const doc = html;
+        console.log('acccessed on tab:', tabId)
+        console.log(doc);
+        console.log(typeof tabId)
+        console.log(typeof doc)
+        sendDataToBackend(tabId, doc)
+      })
+      .catch(error => {
+        console.error('Error fetching or parsing the HTML:', error);
+      });
+}
+
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
 
 
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  // Get the information about newly accessed webpage
   if (info.status === 'complete') {
     const url = new URL(tab.url);
-
     // Fetch the HTML content of the webpage
-    fetch(url,{method: 'GET'})
-      .then(response => response.text())
-      .then(html => {
-        // Parse the HTML content using DOMParser
-        const doc = html;
-        console.log('acccessed', tabId)
-        console.log(doc);
-      })
-      .catch(error => {
-        console.error('Error fetching or parsing the HTML:', error);
-      });
+    fetchUrl(tabId,url)
   }
 });
 
@@ -32,23 +61,9 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
   chrome.tabs.get(tabId, function(tab) {
     // Check if the tab is accessible
     if (!chrome.runtime.lastError && tab && tab.url.startsWith("http")) {
-      const pageUrl = tab.url;
-      console.log("URL of the active page:", pageUrl);
-      
+      const url = tab.url;
       // Fetch the HTML content of the webpage
-      fetch(tab.url,{method: 'GET'})
-      .then(response => response.text())
-      .then(html => {
-        // Parse the HTML content using DOMParser
-        const doc = html;
-        console.log('acccessed',tabId)
-        console.log(doc);
-      })
-      .catch(error => {
-        console.error('Error fetching or parsing the HTML:', error);
-      });
-
-      // Now you can perform actions with the URL, such as scraping content or any other logic you need
+      fetchUrl(tabId,url)
     }
   });
 });
